cdb4408c08258a168de1a31e8280a23c86d1beb4
Hano2001 <haraldnorlin93@gmail.com>
2024-05-19 17:00:01 +0200

    second api fetch test in progress

--- src/components/fetch/fetch.test.ts
+++ src/components/fetch/fetch.test.ts
@@ -5,4 +5,8 @@ describe("Testing the API", () => {
     const result = await FetchFunction();
     expect(result.length).toEqual(30);
   });
+  test("Types", async () => {
+    const result = await FetchFunction();
+    expect(true).toEqual(true)
+  })
 });
