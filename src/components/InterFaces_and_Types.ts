2f43c9b4bb18c2643e36b283ba90efbf3bb55dde
Hano2001 <haraldnorlin93@gmail.com>
2024-05-29 12:14:32 +0200

    interface in category, polishing code

--- src/components/InterFaces_and_Types.ts
+++ src/components/InterFaces_and_Types.ts
@@ -1,22 +1,28 @@
 export  interface IValues {
-    name:string,
+    name:string;
     id:number
   };
 export type Tdata = Array<string>
 
 export interface Imain  {
-  idMeal : string,
-  strMeal: string,
-  strCategory: string,
-  strArea: string,
-  strInstructions:string,
-  strMealThumb:string,
-  strTags:string,
-  strYoutube?: string,
+  idMeal : string;
+  strMeal: string;
+  strCategory: string;
+  strArea: string;
+  strInstructions:string;
+  strMealThumb:string;
+  strTags:string;
+  strYoutube?: string;
   strSource?:string
 }
 
 export interface IRecipeObject {
   [key:string] :string
 }
-  // export type TJson Record<string,unknown>
+
+export interface IcategoryMealObject {
+  strMeal:string;
+  strMealThumb:string;
+  idMeal:string
+}
+  // export type TJson Record<string;unknown>
--- src/pages/Category.tsx
+++ src/pages/Category.tsx
@@ -2,12 +2,13 @@ import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { RecipeFetchFunction } from "../components/fetch/Fetch";
 import DishImage from "../components/DishImage";
+import { IcategoryMealObject } from "../components/InterFaces_and_Types";
 
 export default function Category() {
-  const [data, setData] = useState<any>([]);
+  const [data, setData] = useState<IcategoryMealObject | null>(null);
   const params:any = useParams();
   useEffect(() => {
-    if (data.length === 0) {
+    if (data === null) {
       RecipeFetchFunction(params.c).then(setData).then;
     }
   }, []);
@@ -16,14 +17,14 @@ export default function Category() {
     RecipeFetchFunction(params.c).then(setData).then;
   }
 
-  const recipeEndpoint = "/recipe/" + data.idMeal;
+  const recipeEndpoint = "/recipe/" + data?.idMeal;
 
   return (
     <div>
       <h1>Here is your random Recipe! Happy Cooking!</h1>
-      <h2>{data.strMeal}</h2>
+      <h2>{data?.strMeal}</h2>
       <button onClick={FetchNewRecipe}>Generate new Recipe</button>
-      <DishImage imgUrl={data.strMealThumb}/>
+      <DishImage imgUrl={data?.strMealThumb!}/>
       <Link to={recipeEndpoint}>Recipe Details</Link>
       <Link to="/">Back to Categories</Link>
     </div>
